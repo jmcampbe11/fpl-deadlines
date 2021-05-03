b66199659c4e7c7ddb0510e0b97afe67551ad3b8
John Campbell <43732873+jmcampbe11@users.noreply.github.com>
2021-05-02 21:58:18 -0300

    Change JSON filepath and remove a TODO comment

--- import_deadlines.py
+++ import_deadlines.py
@@ -1,14 +1,14 @@
-# Import the Fantasy Premier League (FPL) JSON file
+# Imports the Fantasy Premier League (FPL) API JSON file
+# Find JSON file at: https://fantasy.premierleague.com/api/bootstrap-static/
 # Exports an .ics file that contains the FPL's Gameweek deadlines 
 
 import json
 #import requests # TODO: retrieve JSON from website instead of downloaded file.
 import icalendar
-from datetime import timedelta
-import os
 import re
+from datetime import timedelta
 
-with open("/Users/John/Documents/download.json") as readFile:
+with open("./download.json") as readFile:
     data = json.load(readFile)
 
 # Define calendar
@@ -21,26 +21,20 @@ keys = ["name", "deadline_time"]
 # The "range" function's "end" parameter is exclusive.
 for iGameweek in list(range(0,38)):
     for key in keys:
-        print(data["events"][iGameweek][key])
         # Add Gameweek Deadline event to calendar
         if key == "deadline_time":
             # Remove non-alphanumeric characters
             gwDeadline = re.sub(r'\W+', '', data["events"][iGameweek]["deadline_time"])
 
+            # Create deadline event
             event = icalendar.Event()
             event['dtstart'] = gwDeadline
             event['dtend'] = gwDeadline
             event.add('summary', data["events"][iGameweek]["name"] + " Deadline")
-            # add gw number to UID
             uid = "fpl2021-" + str(iGameweek + 1)
             event.add("UID", uid)
 
-            # Simple Calendar Specific 
-            event.add("CATEGORY_COLOR", -16452352)
-            #event.add("CATEGORIES", "Reminder") # iCalendar formats stirng as - R,e,m,i,n,d,e,r
-
-            # TODO: Add alarm (2 hours before event)
-
+            # Set alarm for 2 hours before deadline
             alarm = icalendar.Alarm()
             alarm.add("ACTION", "DISPLAY")
             alarmDelta = timedelta(hours = -2)
@@ -48,14 +42,9 @@ for iGameweek in list(range(0,38)):
             alarm.add("DESCRIPTION", "Reminder")
 
             event.add_component(alarm)
-
             cal.add_component(event)
 
 # Write to .ics file
 f = open("FPL_GW_Deadlines.ics", "wb")
 f.write(cal.to_ical())
 f.close()
-
-# TODO: add code to Github
-
-
